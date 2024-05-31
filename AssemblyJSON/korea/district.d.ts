b79807f747374a16daf09b4375465362e42b0997
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-05-31 13:52:44 +0900

    refactor: remove AssemblyJSON

--- AssemblyJSON/korea/district.d.ts
+++
@@ -1,8 +0,0 @@
-export interface District {
-  /** @description Unique for each generation */
-  시도_선거구명: string;
-  시도: string;
-  선거구명: string;
-  /** @description Explanation */
-  선거구역: string;
-}
--- AssemblyJSON/util.d.ts
+++
@@ -1,7 +0,0 @@
-export type Variable<T> = {
-  /** @description Include */
-  start: Date;
-  /** @description Exclude */
-  end: Date;
-  value: T;
-}[];
